54727e783d90e5e2dd71fb628f7977d1f249f40f
dewanhasan99 <g00410156@atu.ie>
2024-12-25 12:14:09 +0000

    First Commit

---
+++ src/main/java/ie/atu/studentservicem1/StudentServiceM1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.studentservicem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceM1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServiceM1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/studentservicem1/StudentServiceM1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.studentservicem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceM1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
