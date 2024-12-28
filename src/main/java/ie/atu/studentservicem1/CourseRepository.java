86817494847e6d37200b33400bfee1fdd63e01c2
dewanhasan99 <g00410156@atu.ie>
2024-12-28 17:30:44 +0000

    Bug Fixes

--- src/main/java/ie/atu/studentservicem1/CourseRepository.java
+++ src/main/java/ie/atu/studentservicem1/CourseRepository.java
@@ -1,9 +1,11 @@
 package ie.atu.studentservicem1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
 
     List<Course> findByStudentId(Long studentId);
